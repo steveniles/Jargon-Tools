aa9f78bc7cd5b808ca3d030b1d1adbf020ce39a5
Steve Niles <steve.niles@gmail.com>
2025-07-14 16:19:23 -0400

    Fix header link clickable area bug

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,16 @@ import { Link } from "react-router";
 export default function Header({ title }: { title: string }) {
   return (
     <header className="grid w-full grid-cols-[1fr_auto_1fr] gap-4">
-      <Link className="font-[Glory] tracking-tighter" to="/" viewTransition>
-        <span style={{ viewTransitionName: "jargon" }} className="font-light">
-          Jargon
-        </span>
-        <span style={{ viewTransitionName: "tools" }} className="font-medium">
-          Tools
-        </span>
-      </Link>
+      <span>
+        <Link className="font-[Glory] tracking-tighter" to="/" viewTransition>
+          <span style={{ viewTransitionName: "jargon" }} className="font-light">
+            Jargon
+          </span>
+          <span style={{ viewTransitionName: "tools" }} className="font-medium">
+            Tools
+          </span>
+        </Link>
+      </span>
       <h1 className="font-[Roboto] font-bold">{title}</h1>
     </header>
   );
